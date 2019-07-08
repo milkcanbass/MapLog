23a45d6f603d06a0df3890747dde607932c83647
milkcan <milkcanbass@gmail.com>
2019-07-08 18:55:01 -0400

    developing

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -1,15 +1,40 @@
 import React, { useState, Fragment } from "react";
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import img from "../img/frontend.png";
+
+//Since react-maps-react library doesn't support to attach event handler to InforWindow dynamic content.
+//To solve this, render InfoWindow children into a DOM node in order to prevent losing React Context.
+
+import InfoWindowEx from "./layout/InfoWindowEx";
 
 const mapStyles = {
   width: "100%",
   height: "100%",
   position: "relative",
   top: "10px"
+};
+const imgStyle = {
+  maxWidth: "400px",
+  minWidth: "200px",
+  maxHeight: "auto"
+};
+
+const infoWindowStyle = {
+  maxWidth: "400px",
+  minWidth: "200px",
+  height: "auto"
+};
+
+const button = {
+  background: "#3498db",
+  maxWidth: "400px",
+  padding: "15px 100px",
+  position: "relative",
+  margin: "10px 0",
+  borderRadius: "3px"
 };
 
 //Get lat and lng by clicking map
@@ -38,6 +63,9 @@ const MapContainer = props => {
     });
   };
 
+  const [textAreaState, setTextAreaState] = useState({ textArea: "" });
+  const { textArea } = textAreaState;
+
   //Add marker
   const onMapClicked = (props, map, clickEvent) => {
     const { latLng } = clickEvent;
@@ -56,6 +84,15 @@ const MapContainer = props => {
         activeMarker: null
       });
     }
+  };
+
+  const onChange = e => {
+    setTextAreaState({ [e.target.name]: e.target.value });
+  };
+
+  //This is submitButton to send message on text area
+  const sendMessage = () => {
+    console.log(textAreaState);
   };
 
   return (
@@ -82,19 +119,34 @@ const MapContainer = props => {
           // title={markerState.title}
           onClick={onMarkerClick}
         />
-        <InfoWindow
+        <InfoWindowEx
           marker={infoWindowState.activeMarker}
           visible={infoWindowState.showingInfoWindow}
         >
-          <div>
+          <div style={infoWindowStyle}>
             <center>
-              <h1>Hello</h1>
-              <h1>Hello</h1>
-              <img src={img} />
-              <input type="text" />
+              <h1>{markerState.title}</h1>
+              <p>updated:2019 12/12</p>
+              <div>
+                <p>
+                  <img src={img} className="img" style={imgStyle} />
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </p>
+              </div>
+
+              <button type="button" style={button} onClick={sendMessage}>
+                <a href={`mailto:${markerState.name}`}>Send mail</a>
+              </button>
             </center>
           </div>
-        </InfoWindow>
+        </InfoWindowEx>
       </Map>
     </Fragment>
   );
